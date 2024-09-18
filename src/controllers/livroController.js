d774247096a727a39873104a2fd45facc6a91f10
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-18 18:14:32 -0300

    refactor(app): integrate routes and controllers for better structure to add livro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -7,6 +7,15 @@ class LivroController {
     res.status(200).json(listaLivros);
   }
 
+  static async cadastrarLivro(req, res) {
+    try {
+      const novoLivro = await livro.create(req.body)
+      res.status(201).json({ message: "cadastrado com sucesso", livro: novoLivro });
+    } catch (error) {
+      res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  };
+
 };
 
 export default LivroController;
