cd38c960d5679bccab9b0ecca7ece33d220d4b7c
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-17 23:05:46 -0300

    feat(config): add dotenv for sensitive environment variables

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function conectaNaDataBase() {
-  mongoose.connect("mongodb+srv://arielspencer:UCyNiYm9VQA59t7x@cluster0.0jege.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
 
   return mongoose.connection;
 };
