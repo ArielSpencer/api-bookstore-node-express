c50819aae3d8a1288086e52177115e8c4838c0d7
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-18 18:30:57 -0300

    refactor(db): improve listing and updating to books with controllers and routers

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,9 +3,23 @@ import livro from "../models/Livro.js";
 class LivroController {
 
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-  }
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  };
+
+  static async listarLivroPorID(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (error) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+    }
+  };
 
   static async cadastrarLivro(req, res) {
     try {
@@ -16,6 +30,15 @@ class LivroController {
     }
   };
 
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "livro atualizado" });
+    } catch (error) {
+      res.status(500).json({ message: `${erro.message} - falha na atualização do livro` });
+    }
+  };
 };
 
 export default LivroController;
