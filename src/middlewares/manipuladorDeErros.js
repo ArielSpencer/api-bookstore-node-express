64bf9c2e2da67e88fa0ca34e13bba22d06f8d501
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-26 13:55:49 -0300

    feat(db): add pagination to book listing

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import ErroBase from "../errors/ErroBase.js";
 import ErroValidacao from "../errors/ErroValidacao.js";
 import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
-import NaoEncontrado from "../errors/NaoEncontrado.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(error, req, res, next) {
@@ -10,7 +9,7 @@ function manipuladorDeErros(error, req, res, next) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (error instanceof mongoose.Error.ValidationError) {
     new ErroValidacao(error).enviarResposta(res);
-  } else if (error instanceof NaoEncontrado) {
+  } else if (error instanceof ErroBase) {
     error.enviarResposta(res);
   } else {
     new ErroBase().enviarResposta(res);
